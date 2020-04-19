49112aea5becba68713f8a2c9cea62745a7b8a52
rmbackoTU-dev <rmbacko92@gmail.com>
2020-04-18 22:17:45 -0400

    Created Initial GUI Components

---
+++ app/src/main/java/com/example/basic_geo_quiz_app/QuizStartActivity.java
@@ -0,0 +1,13 @@
+package com.example.basic_geo_quiz_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuizStartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+}
---
+++ app/src/main/java/com/example/basic_geo_quiz_app/StatsPage.java
@@ -0,0 +1,14 @@
+package com.example.basic_geo_quiz_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StatsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats_page);
+    }
+}
